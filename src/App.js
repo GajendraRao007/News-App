3bc50c2decfe1840623c94b2db754b62e4db12dc
GajendraRao007 <raogajendra27@gmail.com>
2024-04-06 15:02:47 +0530

    News app using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
+import { useState } from 'react';
+import News from './News';
 
 function App() {
+
+  let [articles,setArticles] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://newsapi.org/v2/everything?q=india&from=2024-04-04&apiKey=08167649afd14634a754d23691fd556b")
+    .then((response)=>response.json())
+    .then((news)=>{
+      setArticles(news.articles);
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+          <h1>News Nest</h1>
+          <input type='search' placeholder='Search News'/>
       </header>
+      <section className='news-articles'>
+        {
+          articles.map((article)=>{
+            return(
+
+              <News/>
+            )
+          })
+        }
+      </section>
     </div>
   );
 }
---
+++ src/News.jsx
@@ -0,0 +1,19 @@
+function News(){
+
+    return(
+        <div className="news">
+            <div className="news-img">
+                <img src="" alt="" srcset="" />
+            </div>
+                <h1 className="title">Title</h1>
+                <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate modi dignissimos alias! Similique ab odit maiores perferendis numquam, natus nesciunt?</p>
+                <a href="#" className="readmore-btn">Read More</a>
+                <div className="source">
+                    <p>Gajendra Rao</p>
+                    <p>InfraSieve</p>
+                </div>
+        </div>
+    )
+}
+
+export default News;
