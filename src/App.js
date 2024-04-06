2a74f7a18e1c7fb4fe353bff42e3761ab8948ea8
GajendraRao007 <raogajendra27@gmail.com>
2024-04-06 19:50:04 +0530

    Search News Feature added

--- src/App.js
+++ src/App.js
@@ -6,29 +6,45 @@ import News from './News';
 function App() {
 
   let [articles,setArticles] = useState([]);
+  let [category,setCategory] = useState("india");
 
   useEffect(()=>{
-    fetch("https://newsapi.org/v2/everything?q=india&from=2024-04-05&apiKey=08167649afd14634a754d23691fd556b")
+    fetch(`https://newsapi.org/v2/everything?q=${category}&from=2024-04-05&apiKey=08167649afd14634a754d23691fd556b`)
     .then((response)=>response.json())
     .then((news)=>{
       setArticles(news.articles);
+      console.log(news.articles);
     })
-  },[])
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[category])
 
   return (
     <div className="App">
       <header className='header'>
           <h1>News Nest</h1>
-          <input type='search' placeholder='Search News'/>
+          <input type='search' onChange={(event)=>{
+            if(event.target.value!=""){
+              setCategory(event.target.value);
+            }else{
+              setCategory("india")
+            }
+          }} placeholder='Search News'/>
       </header>
       <section className='news-articles'>
         {
+
+          articles.length!==0?
+
           articles.map((article)=>{
             return(
 
               <News article={article}/>
             )
           })
+          :
+          <h1>News Not Found For Searched Text</h1>
         }
       </section>
     </div>
--- src/News.jsx
+++ src/News.jsx
@@ -3,7 +3,11 @@ console.log(props);
     return(
         <div className="news">
             <div className="news-img">
-            <img src={props.article.urlToImage}/>
+                {
+                    props.article.urlToImage!==null?
+                    <img src={props.article.urlToImage}/>:
+                    <img src="https://imgs.search.brave.com/S3T3B8nahnuwGHbVWX6JrrDTeQ9cKkw7nxC8Vabl9lU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzg5LzU1LzE1/LzM2MF9GXzg5NTUx/NTk2X0xkSEFaUnd6/M2k0RU00SjBOSE5I/eTJoRVVZRGZYYzBq/LmpwZw"/>
+                }
             </div>
                 <h1 className="title">{props.article.title}</h1>
                 <p className="description">{props.article.description?.substring(0,100).concat('...')} <a href={props.article.url} target="_blank" className="readmore-btn">Read More</a></p>
